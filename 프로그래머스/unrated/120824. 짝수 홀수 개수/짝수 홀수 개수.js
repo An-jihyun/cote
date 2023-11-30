8b6b556e42c527b26f2d8952cc952a04cbc7566c
An-jihyun <76841930+An-jihyun@users.noreply.github.com>
2023-11-30 22:25:28 +0900

    [level unrated] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -0,0 +1,13 @@
+function solution(num_list) {
+  // reduce 함수를 사용하여 배열을 순회하면서 짝수와 홀수의 개수를 계산
+  return num_list.reduce(
+    (counts, number) => {
+      // number가 짝수인 경우 counts 배열의 첫 번째 요소를 증가, 홀수인 경우 두 번째 요소를 증가
+      counts[number % 2]++;
+      // 최종 결과 반환
+      return counts;
+    },
+    // 초기값으로 [0, 0] 배열을 사용, 첫 번째 요소는 짝수 개수, 두 번째 요소는 홀수 개수
+    [0, 0]
+  );
+}
