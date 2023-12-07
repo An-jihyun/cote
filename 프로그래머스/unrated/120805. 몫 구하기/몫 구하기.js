353cb6bc3318ed2dbfe0ac5e87c8b98934bd36c1
An-jihyun <76841930+An-jihyun@users.noreply.github.com>
2023-12-07 23:59:23 +0900

    [level unrated] Title: 몫 구하기, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120805. 몫 구하기/몫 구하기.js
+++ 프로그래머스/unrated/120805. 몫 구하기/몫 구하기.js
@@ -1,4 +1,4 @@
-function solution(num1, num2) {
-    var answer = Math.floor(num1 / num2);
-    return answer;
-}
+//20231125
+//20231207
+const solution = (num1, num2) => Math.floor(num1 / num2);
+//화살표 함수로 간결하게 정리
