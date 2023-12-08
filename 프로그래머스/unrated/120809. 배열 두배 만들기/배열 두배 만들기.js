c1e12c660cb33df2ee8349f2d2f4221fae3b0418
An-jihyun <76841930+An-jihyun@users.noreply.github.com>
2023-12-08 23:11:29 +0900

    [level unrated] Title: 배열 두배 만들기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/unrated/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,7 +1,5 @@
+//20231126
+//20231208
+const solution = (numbers) => numbers.map(x => x * 2);
 //map을 사용하여 배열의 각 요소에 연산
 //화살표 함수를 사용하여 간결하게 가독성 높이기
-//var 대신 const를 사용하여 변수의 재할당을 방지
-function solution(numbers) {
-    const answer = numbers.map(x=> x * 2);
-    return answer;
-}
