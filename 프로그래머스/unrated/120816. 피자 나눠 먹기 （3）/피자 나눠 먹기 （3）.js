a3234ce09d44372909a7668334a4a3cf595232e9
An-jihyun <76841930+An-jihyun@users.noreply.github.com>
2023-12-13 12:29:05 +0900

    [level unrated] Title: 피자 나눠 먹기 (3), Time: 0.02 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/unrated/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,3 +1,5 @@
-function solution(slice, n) {
-    return Math.ceil(n/slice);
-}
+//20231128
+//20231213
+const solution = (slice, n) => Math.ceil(n/slice);
+
+//피자 판은 소수 판이 될 수 없는데 나눗셈의 결과가 소수점일 때 Math.ceil()로 올림하여 정수로 만들기
