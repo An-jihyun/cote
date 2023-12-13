3a4f2599137c3c20958986a58082fb59e026604f
An-jihyun <76841930+An-jihyun@users.noreply.github.com>
2023-12-13 12:38:30 +0900

    [level unrated] Title: 배열의 평균값, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.js
+++ 프로그래머스/unrated/120817. 배열의 평균값/배열의 평균값.js
@@ -1,3 +1,7 @@
-function solution(numbers) {
-    return numbers.reduce((acc, cur) => acc + cur) / numbers.length
-}
+//20231128
+//20231213
+const solution = (numbers) => numbers.reduce((a,b) => a+b, 0) / numbers.length;
+
+// reduce로 배열의 각 요소를 누적하여 합 계산 (초기값 0, a=누적값, b=각 배열 요소)
+
+// / numbers.length로 계산된 합을 배열의 길이로 나누어 평균 계산
