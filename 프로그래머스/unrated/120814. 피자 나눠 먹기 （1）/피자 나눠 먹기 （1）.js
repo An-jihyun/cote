480d7f496b4899e0f426cc6fcf357b8167715859
An-jihyun <76841930+An-jihyun@users.noreply.github.com>
2023-12-13 12:17:36 +0900

    [level unrated] Title: 피자 나눠 먹기 (1), Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
+++ 프로그래머스/unrated/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.js
@@ -1,5 +1,6 @@
-//Math.ceil로 나눈 결과를 올림하여 최소한의 피자 판 수를 구하기
+//20231128
+//20231213
+const solution = (n) => Math.ceil(n / 7);
 
-function solution(n) {
-    return Math.ceil(n / 7);
-}
+//Math.ceil로 나눈 결과를 올림하여 최소한의 피자 판 수를 구하기
+//화살표 함수를 사용하여 간결하게 정리
