80e5977180ca56f9243e265c73e2746e3cdf33f0
An-jihyun <76841930+An-jihyun@users.noreply.github.com>
2023-12-14 19:56:51 +0900

    [level unrated] Title: 특정 문자 제거하기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.js
+++ 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.js
@@ -1,3 +1,4 @@
-function solution(myString, letter) {
-    return myString.split(letter).join('');
-}
+//20231201
+//20231214
+const solution = (myString, letter) => myString.split(letter).join('');
+//split 메서드를 사용하여 문자열을 특정 문자 letter를 기준으로 나누기. 이렇게 하면 해당 문자가 제거된 배열이 생성
