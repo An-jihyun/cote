e9db2192bc03e100f8976fceb3d2eec99764f7d1
An-jihyun <76841930+An-jihyun@users.noreply.github.com>
2023-12-14 19:47:31 +0900

    [level unrated] Title: 문자열 뒤집기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.js
+++ 프로그래머스/unrated/120822. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,3 +1,6 @@
-function solution(my_string) {
-  return my_string.split('').reverse().join('');
-}
+//20231130
+//20231214
+const solution= (my_string) => my_string.split('').reverse().join('');
+//split로 각 문자를 배열로 반환
+//그것을 reverse 시키기
+//다시 join을 사용하여 한 단어로 합치기
