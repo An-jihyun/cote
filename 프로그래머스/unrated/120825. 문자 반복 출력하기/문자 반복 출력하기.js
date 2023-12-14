e1dc5291b29908c16dfcb1e2d59f5296279b58f4
An-jihyun <76841930+An-jihyun@users.noreply.github.com>
2023-12-14 19:54:50 +0900

    [level unrated] Title: 문자 반복 출력하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.js
+++ 프로그래머스/unrated/120825. 문자 반복 출력하기/문자 반복 출력하기.js
@@ -1,3 +1,8 @@
+//20231130
+//20231214
 function solution(my_string, n) {
   return my_string.split('').map(char => char.repeat(n)).join('');
 }
+
+//split로 각 문자를 쪼개 배열로 반환
+//map을 사용해 전체 각각에 m만큼 반복하게 하여 join으로 합치기
