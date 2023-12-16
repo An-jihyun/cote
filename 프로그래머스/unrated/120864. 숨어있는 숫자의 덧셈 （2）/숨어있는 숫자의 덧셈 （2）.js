6dab4bc675cbeb1288e49f2b717f3898c9012214
An-jihyun <76841930+An-jihyun@users.noreply.github.com>
2023-12-16 15:33:54 +0900

    [level unrated] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -0,0 +1,12 @@
+function solution(my_string) {
+    // 정규표현식을 사용하여 문자열에서 자연수 추출
+    const numbers = my_string.match(/\d+/g);
+
+    // 자연수가 없는 경우 0을 반환
+    if (!numbers) {
+        return 0;
+    }
+
+    // 추출된 자연수들의 합을 계산하여 반환
+    return numbers.reduce((sum, num) => sum + parseInt(num), 0);
+}
