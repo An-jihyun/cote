ec03a429267fef9f9cbdcb54ee6c3ce48c573f3a
An-jihyun <76841930+An-jihyun@users.noreply.github.com>
2023-12-18 23:37:41 +0900

    [level unrated] Title: 이진수 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120885. 이진수 더하기/이진수 더하기.js
@@ -0,0 +1,9 @@
+function solution(bin1, bin2) {
+    // 이진수를 10진수로 변환하여 합 계산
+    const decimalSum = parseInt(bin1, 2) + parseInt(bin2, 2);
+
+    // 10진수 합을 이진수로 변환하여 반환
+    const binarySum = decimalSum.toString(2);
+
+    return binarySum;
+}
